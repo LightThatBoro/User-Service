31e03432b2c8e0827294ade5a70d22276262d658
sarcastic-verma <shivamthegreat.sv@gmail.com>
2022-01-26 00:05:08 +0530

    :art: added handler for sns events

---
+++ src/functions/service-name-consumer.ts
@@ -0,0 +1,31 @@
+import { Context, SNSEvent } from "aws-lambda";
+import subscribers from "../subscribers";
+import { ISubscriberParams } from "../types";
+import getConnection from "../utils/get-connection";
+import MAIN_LOGGER from "../utils/logger";
+
+export const handler = async(event: SNSEvent, ctx: Context) => {
+	const logger = MAIN_LOGGER.child({ requestId: ctx.awsRequestId });
+	const db = await getConnection();
+
+	return await Promise.all(
+		event.Records.map(async({
+			Sns: {
+				Message, MessageAttributes
+			}
+		}) => {
+			const eventName = MessageAttributes["eventName"].Value;
+
+			if(Object.keys(subscribers).includes(eventName)) {
+				logger.info({ event, data: Message.slice(0, 200) }, "received event");
+
+				const data = JSON.parse(Message);
+				const userId = MessageAttributes["userId"].Value;
+
+				// calls the subscriber mapping to the event
+				await subscribers[eventName](<ISubscriberParams> { userId, data, db });
+			}
+		}
+		)
+	);
+};
