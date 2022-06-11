ee48500e1078fcb2d8024a788e9d80132353a101
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2022-06-11 17:18:58 +0530

    create insert user controller

---
+++ src/routes/insert-user.ts
@@ -0,0 +1,21 @@
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
+import { Handler } from "../utils/make-api";
+
+// the "Handler" type automatically does type checks for the response as well
+const handler: Handler<"insertUser"> = async(
+	data, // the parameters, query & request body are automatically combined
+	{ db },
+	{ firebaseAuth } // user that made the request
+) => {
+	const userRepo = getRepository(User);
+
+	const res = await userRepo.save({
+		...data
+	});
+
+	return res;
+
+};
+
+export default handler;
